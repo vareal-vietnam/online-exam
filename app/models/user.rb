7f25de0b14fe7b7c5d2f1cc4eb1b0653d1f89ca3
mai-123 <52277046+mai-123@users.noreply.github.com>
2019-07-16 08:13:09 +0700

    Create User model (#3)
    * create model and table user

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  has_secure_password
+
+  validates :name, presence: true, lenth: {maximum: 50, minimum: 3}
+  validates :email, presence: true, length: {maximum: 255},
+                    format: {with: VALID_EMAIL_REGEX},
+                    uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 6}
+end
