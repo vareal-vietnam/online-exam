efe4f1e957f88f0e724840a762994fd239db03c2
HaiTranVar <51992131+HaiTranVar@users.noreply.github.com>
2019-07-18 15:52:43 +0700

    close #19 Design Logout (#20)

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
