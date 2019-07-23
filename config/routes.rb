c36115a7e831d8b706472786d47e29e3f4797f52
mai-123 <52277046+mai-123@users.noreply.github.com>
2019-07-23 14:03:06 +0700

    close #1 delete user (#2)
    * close #30 delete user
    
    * fix i18n
    
    * fix delete user self
    
    * fix icon delete user
    
    * remove comment in routes.rb
    
    * remove if in destroy user

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   get "/logout", to: "sessions#destroy"
   resources :password_resets, only: [:new, :create, :edit, :update]
-  resources :users, only: [:new, :create, :edit, :update, :index, :show]
+  resources :users
 end
