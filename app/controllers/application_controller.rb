2b251032a60a1b31f5ad6cddf65877e9ed49a6e3
mai123 <pham.thi.mai@vareal.vn>
2019-07-23 11:02:26 +0700

    fix delete user self

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def check_is_admin_permission
     return if logged_in? && current_user.is_admin?
-    flash[:danger] = t "not_permission"
+    flash[:danger] = t "error_403"
     redirect_to root_path
   end
 end
