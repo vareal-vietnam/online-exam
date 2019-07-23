7efac404d6e91adfa8381112f4495b2a738b887c
mai123 <pham.thi.mai@vareal.vn>
2019-07-23 13:57:34 +0700

    remove comment in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,4 @@ Rails.application.routes.draw do
   get "/logout", to: "sessions#destroy"
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
