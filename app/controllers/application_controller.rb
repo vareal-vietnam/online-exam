33917d986fda11ec7eb050dfee1305a7afb5bb5c
mai123 <pham.thi.mai@vareal.vn>
2019-07-23 16:57:44 +0700

    fix error when user not login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   def check_is_admin_permission
-    check_is_logged_in
-    return if current_user.is_admin?
+    return if check_is_logged_in || current_user.is_admin?
     flash[:danger] = t "error_403"
     redirect_to root_path
   end
@@ -11,6 +10,6 @@ class ApplicationController < ActionController::Base
   def check_is_logged_in
     return if logged_in?
     flash[:danger] = t "not_loggin"
-    redirect_to login_path
+    redirect_to login_path and return true
   end
 end
