c7ab0804f6196159f3f8efd39c811ef41b21d187
mai123 <pham.thi.mai@vareal.vn>
2019-07-25 13:15:14 +0700

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,19 @@
 10.times do |n|
-  User.create! name: Faker::Name.name, email: "user#{n+1}@vareal.vn", password: "123456", password_confirmation: "123456"
+  User.create! name: Faker::Name.name,
+    email: "user#{n+1}@vareal.vn",
+    password: "123456",
+    password_confirmation: "123456"
 end
 
-10.times do
-  Test.create! kind: [:git, :rails].sample, time: [500, 600, 400].sample, name: ["Git advance", "Git basic", "Rails basic", "Rails advance"].sample
-end
+Test.create! kind: :git, time: 600, name: "Git advance"
+Test.create! kind: :git, time: 500, name: "Git basic"
+Test.create! kind: :rails, time: 400, name: "Rails basic"
+Test.create! kind: :rails, time: 700, name: "Rails advance"
 
 30.times do
-  Result.create! score: rand(1..10), user: User.all.sample, test: Test.all.sample
+  Result.create! score: rand(1..10),
+    user: User.all.sample,
+    test: Test.all.sample
 end
 
 60.times do
