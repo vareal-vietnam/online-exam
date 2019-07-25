1f19ad6c534c243d4ba50cd04af401f61c389526
mai123 <pham.thi.mai@vareal.vn>
2019-07-25 13:07:24 +0700

    switch task

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,7 @@
 class TestsController < ApplicationController
   before_action :check_is_logged_in, only: [:index, :new, :create]
   before_action :check_is_admin_permission, only: [:new, :create]
+  before_action :get_test, only: [:add_question]
 
   def index
     @tests = Test.all
@@ -23,8 +24,21 @@ class TestsController < ApplicationController
     end
   end
 
+  def add_question
+    @question = Question.new test: @test
+    render "questions/_form"
+  end
+
   private
   def test_params
     params.require(:test).permit :name, :kind, :time
+  end
+
+  def get_test
+    @test ||= Test.find_by id: params[:id]
+    unless @test
+      flash[:danger] = t "error_404"
+      redirect_to root_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   get "/logout", to: "sessions#destroy"
+  get "/add_question/:id", to: "tests#add_question", as: "add_question"
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :users
   resources :tests, only: [:new, :create]
