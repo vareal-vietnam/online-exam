502f06a78a361988785bf26d1020bad3c6aeb151
Tran Hai <tranhai174595218@gmail.com>
2019-07-29 13:15:20 +0700

    Close 3 Delete Question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,12 @@
 class QuestionsController < ApplicationController
-  before_action :correct_question,   only: :destroy
+  before_action :correct_question, only: :destroy
+
   def destroy
     @question.destroy
-    flash[:success] = "Question deleted"
+    flash[:success] = t ".question_deleted"
     redirect_to test_path @question.test
   end
+
   private
   def correct_question
     @question = Question.find_by id: params[:id]
