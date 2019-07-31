091c40db20911dea8c2ad19194095f716703a590
Tran Hai <tranhai174595218@gmail.com>
2019-07-31 20:54:13 +0700

    Close 7 sign up

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,8 +1,7 @@
 class AccountActivationsController < ApplicationController
 
   def edit
-    email_param = {email: params[:email]}
-    user = User.find_by email_param
+    user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.update_attribute(:activated,    true)
       user.update_attribute(:activated_at, Time.zone.now)
