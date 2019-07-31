f41a1eaf900f66b4877c50f91b92b34834768525
mai123 <pham.thi.mai@vareal.vn>
2019-07-31 17:06:34 +0700

    fix error

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,6 @@
 class UserMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
-  def password_reset user
+  def password_reset(user)
     @user = user
-    mail to: @user.email, subject: t("user_mailer.password_reset.subject")
+    mail to: @user.email, subject: t('user_mailer.password_reset.subject')
   end
 end
