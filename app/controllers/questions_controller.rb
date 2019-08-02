ff7b850ef179e4ee2068b769776b603c71d7766d
Tran Hai <tranhai174595218@gmail.com>
2019-08-02 08:31:38 +0700

    Close 4 edit question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -27,7 +27,7 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit :content,
-                                     answers_attributes: %i[id content]
+    params.require(:question).permit(:content,
+                                     answers_attributes: %i[id content])
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -4,7 +4,6 @@ class Question < ApplicationRecord
   belongs_to :test
 
   has_many :answers
-  accepts_nested_attributes_for :answers
 
   accepts_nested_attributes_for :answers,
                                 reject_if: :all_blank, allow_destroy: true
