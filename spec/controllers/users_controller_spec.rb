087c283a1910657ce965a254db472c32d90b5da7
mai123 <pham.thi.mai@vareal.vn>
2019-08-06 14:13:46 +0700

    rubocop

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,17 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe "GET #index" do
-    it "returns a 209 custom status code" do
-      get :index
-      expect(response).to have_http_status(:found)
-    end
-  end
-
-  describe "Get #new" do
-    it "Test" do
-      get :new
-      expect(response.status).to eq(200)
-    end
-  end
 end
