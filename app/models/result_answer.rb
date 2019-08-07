d1f303847d107288b7c27f6ba939df2d0f2d2306
Tran Hai <tranhai174595218@gmail.com>
2019-08-07 16:55:54 +0700

    Fix view result

--- app/models/result_answer.rb
+++ app/models/result_answer.rb
@@ -1,4 +1,6 @@
 class ResultAnswer < ApplicationRecord
+  scope :search, -> (answer){where answer: answer}
+  scope :search_re, -> (result){where result: result}
   belongs_to :answer
   belongs_to :result
 end
