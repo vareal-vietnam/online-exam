71b66109aef0205c704fb7e0669d3212b467a6a6
Tran Hai <tranhai174595218@gmail.com>
2019-08-08 09:36:53 +0700

    Close 5 view n exam result

--- app/models/result_answer.rb
+++ app/models/result_answer.rb
@@ -1,6 +1,5 @@
 class ResultAnswer < ApplicationRecord
-  scope :search, -> (answer){where answer: answer}
-  scope :search_re, -> (result){where result: result}
+  scope :by_result, ->(result) { where result: result }
   belongs_to :answer
   belongs_to :result
 end
--- spec/models/test_spec.rb
+++ spec/models/test_spec.rb
@@ -1,21 +1,21 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Test, type: :model do
   describe 'validation test' do
     it 'ensure kind presence' do
-      test = Test.new(name: "Git Test", time: "600").save
+      test = Test.new(name: 'Git Test', time: '600').save
       expect(test).to eq(false)
     end
     it 'ensure time presence' do
-      test = Test.new(name: "Git Test", kind: :git).save
+      test = Test.new(name: 'Git Test', kind: :git).save
       expect(test).to eq(false)
     end
     it 'only accept integer for field time' do
-      test = Test.new(name: "Git Test Pro", kind: :git, time: "abc").save
+      test = Test.new(name: 'Git Test Pro', kind: :git, time: 'abc').save
       expect(test).to eq(false)
     end
     it 'the right kind test' do
-      test = Test.new(name: "Git Test Pro", kind: :git, time: "600").save
+      test = Test.new(name: 'Git Test Pro', kind: :git, time: '600').save
       expect(test).to eq(true)
     end
   end
