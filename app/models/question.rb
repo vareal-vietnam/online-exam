7cd2e7c93d0fc0e2a2cf3bf6645ba22c14e3b7cf
mai123 <pham.thi.mai@vareal.vn>
2019-08-09 11:28:51 +0700

    fix bug 15 and 11

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,8 +2,7 @@ class Question < ApplicationRecord
   acts_as_paranoid
 
   belongs_to :test
-
-  has_many :answers
+  has_many :answers, dependent: :destroy
 
   accepts_nested_attributes_for :answers,
                                 reject_if: :all_blank, allow_destroy: true
