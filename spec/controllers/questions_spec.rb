3fad3858447e9dbd94980f9507b704a28bb64648
mai123 <pham.thi.mai@vareal.vn>
2019-08-14 10:30:37 +0700

    .

--- spec/controllers/questions_spec.rb
+++ spec/controllers/questions_spec.rb
@@ -6,17 +6,26 @@ RSpec.describe QuestionsController, type: :controller do
   let(:question) { create :question }
 
   describe '#edit' do
-    context 'Action with admin' do
+    context 'Logged in with admin, can find question' do
       before do
         session[:user_id] = admin.id
         get :edit, params: { id: question.id }
       end
 
-      it { is_expected.to use_before_action(:get_question) }
       it { is_expected.to render_template 'edit' }
     end
 
-    context 'Action with user' do
+    context 'Logged in with admin, can not find question' do
+      before do
+        session[:user_id] = admin.id
+        get :edit, params: { id: -1 }
+      end
+
+      it { is_expected.to set_flash }
+      it { is_expected.to redirect_to root_path }
+    end
+
+    context 'Logged with user' do
       before do
         session[:user_id] = user.id
         get :edit, params: { id: question.id }
@@ -26,7 +35,7 @@ RSpec.describe QuestionsController, type: :controller do
       it { is_expected.to redirect_to root_path }
     end
 
-    context 'Action with anonymous' do
+    context 'Not login' do
       before { get :edit, params: { id: question.id } }
 
       it { is_expected.to set_flash }
@@ -36,18 +45,28 @@ RSpec.describe QuestionsController, type: :controller do
 
   describe '#update' do
     let(:question_params) { attributes_for :question }
-    context 'Action with admin' do
+
+    context 'Logged in with admin, can find question' do
       before do
         session[:user_id] = admin.id
         put :update, params: { id: question.id, question: question_params }
       end
 
-      it { is_expected.to use_before_action(:get_question) }
       it { is_expected.to set_flash }
       it { is_expected.to redirect_to test_path(question.test) }
     end
 
-    context 'Action with user' do
+    context 'Logged in with admin, can not find question' do
+      before do
+        session[:user_id] = admin.id
+        put :update, params: { id: -1, question: question_params }
+      end
+
+      it { is_expected.to set_flash }
+      it { is_expected.to redirect_to root_path }
+    end
+
+    context 'Logged in with user' do
       before do
         session[:user_id] = user.id
         put :update, params: { id: question.id }
@@ -57,7 +76,7 @@ RSpec.describe QuestionsController, type: :controller do
       it { is_expected.to redirect_to root_path }
     end
 
-    context 'Action with anonymous' do
+    context 'Not login' do
       before { put :update, params: { id: question.id } }
 
       it { is_expected.to set_flash }
@@ -66,18 +85,27 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe '#destroy' do
-    context 'Action with admin' do
+    context 'Logged in with admin, can find question' do
       before do
         session[:user_id] = admin.id
         delete :destroy, params: { id: question.id }
       end
 
-      it { is_expected.to use_before_action(:get_question) }
       it { is_expected.to set_flash }
       it { is_expected.to redirect_to test_path(question.test) }
     end
 
-    context 'Action with user' do
+    context 'Logged in with admin, can not find question' do
+      before do
+        session[:user_id] = admin.id
+        delete :destroy, params: { id: -1 }
+      end
+
+      it { is_expected.to set_flash }
+      it { is_expected.to redirect_to root_path }
+    end
+
+    context 'Logged in with user' do
       before do
         session[:user_id] = user.id
         delete :destroy, params: { id: question.id }
@@ -87,33 +115,11 @@ RSpec.describe QuestionsController, type: :controller do
       it { is_expected.to redirect_to root_path }
     end
 
-    context 'Action with anonymous' do
+    context 'Not login' do
       before { delete :destroy, params: { id: question.id } }
 
       it { is_expected.to set_flash }
       it { is_expected.to redirect_to '/login' }
-    end
-  end
-
-  describe '#get_question' do
-    context 'Can find question' do
-      before do
-        session[:user_id] = admin.id
-        use_before_action :get_question, params: { id: question.id }
-      end
-
-      it { expect(@question).to eq(question) }
-    end
-
-    context 'Can not find question' do
-      before do
-        session[:user_id] = admin.id
-        get :edit, params: { id: -1 }
-      end
-
-      it { expect(@question).to be_nil }
-      it { is_expected.to set_flash }
-      it { is_expected.to redirect_to root_path }
     end
   end
 end
