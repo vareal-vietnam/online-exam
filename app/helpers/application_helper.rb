7b9d1cf538f3a719026bf1c0fb2d5352f31cd38a
mai123 <pham.thi.mai@vareal.vn>
2019-08-14 11:56:25 +0700

    .

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,12 +7,4 @@ module ApplicationHelper
       page_title + ' | ' + base_title
     end
   end
-
-  # def format_name_test(text, max_character)
-  #   if text.length > max_character
-  #     return "<span title='#{text}'>#{text.slice(0..max_character)}...</span>"
-  #   end
-
-  #   "<span title='#{text}'>#{text}</span>"
-  # end
 end
