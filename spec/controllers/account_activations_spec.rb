11e3e9842b2893901345f573331c903f2675f1cb
mai123 <pham.thi.mai@vareal.vn>
2019-08-15 14:02:19 +0700

    fix comment

--- spec/controllers/account_activations_spec.rb
+++ spec/controllers/account_activations_spec.rb
@@ -2,24 +2,7 @@ require 'rails_helper'
 
 RSpec.describe AccountActivationsController, type: :controller do
   describe '#edit' do
-    context 'Can active' do
-      let(:token) { User.new_token }
-      let(:user) { create :user, :not_active }
-      before do
-        get :edit, params: { id: user.activation_token, email: user.email }
-      end
-      it { is_expected.to set_flash }
-      it { is_expected.to redirect_to root_path }
-    end
-
-    context 'Can not active' do
-      context 'Can not find user' do
-        before { get :edit, params: { id: 1, email: 'abc@gmail.com' } }
-
-        it { is_expected.to set_flash }
-        it { is_expected.to redirect_to login_path }
-      end
-
+    context 'Can find user' do
       context 'User activeted' do
         let(:user) { create :user }
         before { get :edit, params: { id: 1, email: user.email } }
@@ -28,16 +11,35 @@ RSpec.describe AccountActivationsController, type: :controller do
         it { is_expected.to redirect_to login_path }
       end
 
-      context 'User not authenticated' do
-        let(:token) { User.new_token }
-        let(:user) do
-          create :user, :not_active, activation_digest: User.digest(token)
-        end
-        before { get :edit, params: { id: 1, email: user.email } }
+      context 'User not active' do
+        context 'User not authenticated' do
+          let(:user) do
+            create :user, :not_active
+          end
+          before { get :edit, params: { id: 1, email: user.email } }
 
-        it { is_expected.to set_flash }
-        it { is_expected.to redirect_to login_path }
+          it { is_expected.to set_flash }
+          it { is_expected.to redirect_to login_path }
+        end
+
+        context 'User authenticated' do
+          let(:token) { User.new_token }
+          let(:user) { create :user, :not_active }
+          before do
+            get :edit, params: { id: user.activation_token, email: user.email }
+          end
+
+          it { is_expected.to set_flash }
+          it { is_expected.to redirect_to root_path }
+        end
       end
+    end
+
+    context 'Can not find user' do
+      before { get :edit, params: { id: 1, email: 'abc@gmail.com' } }
+
+      it { is_expected.to set_flash }
+      it { is_expected.to redirect_to login_path }
     end
   end
 end
