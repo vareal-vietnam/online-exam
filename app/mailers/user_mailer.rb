56d18581bab40baf0c007c9d785e37e694f8a4b8
mai-123 <52277046+mai-123@users.noreply.github.com>
2019-08-19 10:15:26 +0700

    rspec mailer (#58)
    * rspec mailer
    
    * .
    
    * .
    
    * .

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ApplicationMailer
   #
   def account_activation(user)
     @user = user
-    mail to: @user.email, subject: 'Account activation'
+    mail to: @user.email, subject: t('user_mailer.account_activation.subject')
   end
 
   def password_reset(user)
